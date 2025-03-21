9d59f114c451c42dfd50ee88c5f902b456a76d18
clb-braz <clb.braz@protonmail.com>
2025-03-21 16:15:18 -0300

    Create BusinessException.java

---
+++ src/main/java/com/seap/gestao/visitas/exception/BusinessException.java
@@ -0,0 +1,12 @@
+package com.seap.gestao.visitas.exception;
+
+public class BusinessException extends RuntimeException {
+    
+    public BusinessException(String message) {
+        super(message);
+    }
+    
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
